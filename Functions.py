5c425d7998b11cc6cf0833ae5fc7e15d738a114d
AngelinaYudina <ayudina.bi1903@gmail.com>
2023-01-06 13:34:03 +0300

    06.01.23 E fix

--- Functions.py
+++ Functions.py
@@ -20,14 +20,14 @@ def DLH(G):
     return {"D": D, "L": L, "H": H}
 
 
-def CO_2(G):
+def CO_score_2(G):
     # s2(x) = |L(x)|
     L = DLH(G)['L']
     s2_list = list(map(len, L))
     return s2_list
 
 
-def CO_3(G):
+def CO_score_3(G):
     # s3(x) = |X| - |D(x)|
     card_X = len(G)
     D = DLH(G)['D']
@@ -96,54 +96,46 @@ def ES(G):
 
 
 def E(G):
-    # E = union of supports of all maximal lotteries
-    G = np.array(G)
-    zeros = [0] * (len(G[0]) * 2 + 1)
-    list_add = []
-    for i in range(1, len(G[0]) * 2 + 1):
-        temp = deepcopy(zeros)
-        temp[i] = 1  # 1 pivot
-        list_add.append(temp)
-    z_0 = [0 for i in range(len(G) + 1)]
-    zeros.pop(0)
-    z = [*z_0, -1000, *zeros]
-    z[0] = 1
-    zeros.append(0)
+    # E = объединение носителей всех максимальных лотерей
+    list_ind = [i for i in range(len(G))]
+    # Вектор коэффициентов целевой функции
+    z = [1, *[0 for _ in range(len(G))], -1000, *[0] * (len(G[0]) * 2)]
+    # Матрица коэффициентов левой части
     A = []
-    A_1 = [0, *[1 for _ in range(len(G))], 1, *[0 for _ in range(len(G) * 2)]]
-    A.append(A_1)
+    A_0 = [0, *[1 for _ in range(len(G))], 1, *[0 for _ in range(len(G) * 2)]]
+    A.append(A_0)
     i = 0
     for row in np.vstack((G, G)):
+        to_add = [0] * (len(G[0]) * 2 + 1)
+        to_add[i + 1] = 1
         if i <= len(G) - 1:
-            A_i = [0, *row, *(list_add[i])]
+            A_i = [0, *row, *to_add]
         else:
             temp = deepcopy(row)
             temp[i - len(G)] -= 1
-            A_i = [1, *temp, *(list_add[i])]
+            A_i = [1, *temp, *to_add]
         A.append(A_i)
         i += 1
+    # Вектор коэффициентов правой части
     b = [1, *[0 for _ in range(len(A) - 1)]]
     # Построение модели
     E = LpProblem(name="E", sense=LpMaximize)
-    # Переменные задачи:
-    all_ind = [0]
-    for el in list_ind:
-        all_ind.append(el + 1)
-    to_add_ind = "*"
-    for i in range(len(A[0]) - len(list_ind) - 1):  #######
-        all_ind.append(to_add_ind)
-        to_add_ind += "*"
-    p = [LpVariable(name='p' + str(i), lowBound=0) for i in all_ind]  ######
-    # Добавляем в модель ограничения:
+    # Переменные задачи
+    all_ind = list(map(str, list_ind)) + [str(list_ind[-1] + 1)]
+    for i in range(len(A[0]) - len(list_ind) - 1):
+        all_ind.append("*" * (i + 1))
+    p = [LpVariable(name='p' + i, lowBound=0) for i in all_ind]
+    # Ограничения
     for k in [i for i in range(len(G[0]) * 2 + 1)]:
         E += (lpDot(A[k], p) == b[k]), '(' + str(k + 1) + ')'
-    # Добавляем целевую функцию:
+    # Целевая функция
     E += lpDot(z, p)
-    # Смотрим, что получилось:
-    res = []
+    # Решение задачи
+    E.solve()
+    res = set()
     for v in E.variables():
-        if v.varValue > EPS and "*" not in v.name and v.name != 'p0':  ######
-            res.append(int(v.name.replace('p', '')) - 1)
+        if v.varValue > EPS and "*" not in v.name and v.name != 'p0':
+            res.add(int(v.name.replace('p', '')) - 1)
     return res
 
 
