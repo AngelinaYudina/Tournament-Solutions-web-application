68c723355f18d17fa1c10a50fd0a5d28875cfa02
AngelinaYudina <ayudina.bi1903@gmail.com>
2023-05-01 15:04:17 +0300

    05.03.23 MVP

---
+++ 1_📚Homepage.py
@@ -0,0 +1,155 @@
+import sys
+import os
+import streamlit as st
+import streamlit_ext as ste   # Позволяет скачивать файлы без перезагрузки страницы
+import pandas as pd
+import pandas.io.formats.excel
+from Functions import init_ranking, M_T_generator, game_creator, sorting, CO_2, CO_3, UC_M, ES, E, MC_McK, MC_D, \
+    custom_format
+
+st.set_page_config(
+    page_title="Bibliometric IS",
+    page_icon="📚",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        "Report a bug": "mailto:avyudina_2@edu.hse.ru",
+        "About": """
+        Автор: Ангелина Юдина ([GitHub](https://github.com/AngelinaYudina), [cайт](https://www.hse.ru/staff/ayudina))  
+        Стажер-исследователь в [Международном центре анализа и выбора решений НИУ ВШЭ](https://www.hse.ru/DeCAn/)
+        """
+    }
+)
+
+# Машинный эпсилон для числа с плавающей точкой
+EPS = sys.float_info.epsilon
+
+st.title("Библиометрическая информационная система оценки научных журналов")
+st.write("Пожалуйста, загрузите свой файл. Если у вас нет готового файла, вы можете скачать пример данных и "
+         "работать с ним.")
+st.warning("""
+При загрузке csv файла используйте точку с запятой (;) в качестве разделителя!  
+Убедитесь, что во всех рейтингах наибольшее значение соответствует наилучшей альтернативе!
+""")
+# Выгрузка образца данных
+with open("sample_data.xlsx", "rb") as file:
+    ste.download_button(
+        label="Скачать пример данных",
+        data=file,
+        file_name="sample_data.xlsx"
+    )
+# Загрузка файла
+if "file" not in st.session_state:
+    st.session_state["file"] = None
+file = st.file_uploader("Загрузите файл", type=["csv", "xls", "xlsx"], accept_multiple_files=False)
+st.session_state["file"] = file
+# Чтение файла
+if file is not None:
+    st.success("Файл успешно загружен!")
+    try:
+        df = pd.read_csv(file, delimiter=";")
+    except:
+        df = pd.read_excel(file)
+    # Создание матрицы турнирной игры G на основе данных из df
+    df = df.set_index(df.columns[0])
+    rankings_all = init_ranking(df)
+    M, T, counter = M_T_generator(rankings_all)
+    G = game_creator(M)
+    st.write(f"Количество журналов = {df.shape[0]}. Количество ничьих = {counter}. Исходные ранжирования: "
+             f"{list(df.columns)}")
+    # Построение ранжирований сортировкой, основанной на различных турнирных решениях
+    tournament_solutions = [CO_2, CO_3, UC_M, ES, E, MC_McK, MC_D]
+    sol_names = ["Copeland rule (2 v.)", "Copeland rule (3 v.)", "sorting by UC_M", "sorting by ES", "sorting by E",
+                 "sorting by MC_McK", "sorting by MC_D"]
+    for i in range(len(tournament_solutions)):
+        ranking = sorting(G, tournament_solutions[i])
+        rankings_all[sol_names[i]] = ranking
+    st.write("Ранжирования журналов")
+    st.dataframe(rankings_all)
+    # Расчет коэффициента ранговой корреляции τ_b Кендалла
+    corr = rankings_all.corr(method='kendall')
+    ind = corr.columns
+    rounded_corr = corr.round(3)   # Таблицу с округленными значениями
+    st.write("Значения коэффициента τ_b Кендалла")
+    st.dataframe(rounded_corr)
+    # Построение матрицы строгого мажоритарного отношения на основе таблицы со значениями коэффициента τ_b
+    matrix = [[0 for _ in range(len(ind))] for _ in range(len(ind))]
+    for i in range(len(ind)):
+        for j in range(len(ind)):
+            val_i = 0
+            val_j = 0
+            if i < j:
+                for k in range(len(df.columns)):
+                    if corr.iloc[k, i] - corr.iloc[k, j] > EPS:
+                        val_i += 1
+                    elif corr.iloc[k, i] - corr.iloc[k, j] < - EPS:
+                        val_j += 1
+                if val_i > val_j:
+                    matrix[i][j] = 1
+                elif val_i < val_j:
+                    matrix[j][i] = 1
+    matrix_pd = pd.DataFrame(matrix, index=ind, columns=ind)
+    res = pd.DataFrame(matrix, columns=ind, index=ind)
+    sum_col = []
+    for row in matrix:
+        sum_row = sum(row)
+        sum_col.append(sum_row)
+    res["Copeland score s2"] = sum_col
+    st.write("Матрица строгого мажоритарного отношения (τ_b)")
+    st.dataframe(res)
+    # Сравнение методов построения ранжирований
+    pos = [x + 1 for x in range(len(ind))]
+    res_sorted = res.sort_values(by="Copeland score s2", ascending=False)
+    ind_ranked = res_sorted.index
+    final_ranking = pd.DataFrame(ind_ranked, columns=["Метод"], index=pos)
+    rank = []
+    counter = 1
+    value_prev = res_sorted["Copeland score s2"].values.max()
+    for value in res_sorted["Copeland score s2"]:
+        if value < value_prev:
+            counter += 1
+        rank.append(counter)
+        value_prev = value
+    final_ranking["Ранг"] = rank
+    st.write("Ранжирование ранжирований")
+    final_ranking = final_ranking.set_index("Ранг")
+    st.dataframe(final_ranking)
+    # Подготовка файла с результати работы программы
+    st.write("Все результаты в одном файле:")
+    pandas.io.formats.excel.ExcelFormatter.header_style = None   # Разрешение на форматирование индексов
+    with pd.ExcelWriter("Results.xlsx", engine="xlsxwriter") as writer:
+        rankings_all.to_excel(writer, sheet_name="Rankings")
+        pd.DataFrame(M).to_excel(writer, sheet_name="M", header=False, index=False)
+        pd.DataFrame(T).to_excel(writer, sheet_name="T", header=False, index=False)
+        rounded_corr.to_excel(writer, sheet_name="τ_b")
+        res.to_excel(writer, sheet_name="M (τ_b)")
+        final_ranking.to_excel(writer, sheet_name="Ranking of rankings (τ_b)")
+        # Настройка стиля
+        sheet_Rankings = writer.sheets["Rankings"]
+        sheet_M = writer.sheets["M"]
+        sheet_T = writer.sheets["T"]
+        sheet_t_b = writer.sheets["τ_b"]
+        sheet_M_t_b = writer.sheets["M (τ_b)"]
+        sheet_final_res = writer.sheets["Ranking of rankings (τ_b)"]
+        sheets_list = [sheet_Rankings, sheet_M, sheet_T, sheet_t_b, sheet_M_t_b, sheet_final_res]
+        default_col_width = 8
+        for sheet in sheets_list:
+            # Установка базового стиля
+            sheet.set_column("A1:XFD1048576", default_col_width, custom_format(writer, mode="base"))
+        num_rows = rankings_all.shape[0]
+        num_cols = rankings_all.shape[1]
+        sheet_Rankings.set_row(0, 110, custom_format(writer, mode="header_col", rotation=90))
+        sheet_Rankings.set_column("A1:A1", 50, custom_format(writer, mode="header_row"))
+        sheet_t_b.set_column("A1:A1", 19, custom_format(writer, mode="header_row"))
+        sheet_t_b.set_row(0, 110, custom_format(writer, mode="header_col", rotation=90))
+        sheet_M_t_b.set_column("A1:A1", 19, custom_format(writer, mode="header_row"))
+        sheet_M_t_b.set_row(0, 110, custom_format(writer, mode="header_col", rotation=90))
+        sheet_final_res.set_column("B1:B1", 19, custom_format(writer, mode="base"))
+    # Выгрузка результатов
+    with open("Results.xlsx", "rb") as file:
+        ste.download_button(
+            label="Скачать результаты",
+            data=file,
+            file_name="Results.xlsx"
+        )
+    os.remove("Results.xlsx")
--- Functions.py
+++ Functions.py
@@ -1,14 +1,98 @@
 import sys
+from typing import Callable, Union
 import numpy as np
+from pandas.io.excel._xlsxwriter import ExcelWriter
+import pandas as pd
 from pulp import LpMaximize, LpProblem, lpDot, LpVariable
 
+# Машинный эпсилон для числа с плавающей точкой
 EPS = sys.float_info.epsilon
 
 
-def DLH(G):
-    # D(x) = {y in X | yPx}
-    # L(x) = {y in X | xPy}
-    # H(x) = {y in X | (not xPy) and (not yPx) and (x != y)}
+def init_ranking(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Создает таблицу ранжирований (без пропусков).
+    :param df: таблица рейтингов
+    :return: таблица ранжирований (без пропусков)
+    """
+    rankings = list(df.columns)
+    for ranking in rankings:
+        res = np.zeros(len(df))
+        place = 1
+        maximum = df[ranking].max()
+        while maximum != -100:
+            ind_list = np.where(np.abs(df[ranking] - maximum) <= EPS)
+            for ind in ind_list:
+                df[ranking][ind] = -100
+                res[ind] = place
+            place += 1
+            maximum = df[ranking].max()
+        df[ranking] = res
+    df = df.astype("int")
+    return df
+
+
+def M_T_generator(df: pd.DataFrame) -> tuple[np.ndarray, np.ndarray, int]:
+    """
+    Создает матрицу строго строгого мажоритарного отношения и матрицу отношения равенства голосов.
+    Рассчитает количество пар альтарнатив, находящихся в отношении эквивалентности.
+    :param df: таблица ранжирований (без пропусков)
+    :return: матрица строгого мажоритарного отношения, матрица отношения равенства голосов, количество ничьих
+    """
+    values = df.values
+    M = np.zeros((values.shape[0], values.shape[0]))
+    T = np.zeros((values.shape[0], values.shape[0]))
+    num = 0
+    for i in range(values.shape[0]):
+        for j in range(values.shape[0]):
+            counter = 0
+            for k in range(0, values.shape[1]):  # проход по ранжированиям
+                if values[i][k] < values[j][k]:
+                    counter += 1
+                elif values[i][k] > values[j][k]:
+                    counter -= 1
+            if np.sign(counter) == 1:
+                M[i][j] = 1
+            elif np.sign(counter) == 0 and i != j:
+                T[i][j] = 1
+                T[j][i] = 1
+                num += 1
+    return M.astype(np.int64), T.astype(np.int64), num // 2
+
+
+def game_creator(M: np.ndarray) -> np.ndarray:
+    """
+    Создает матрицу турнирной игры.
+    :param M: матрица строгого мажоритарного отношения.
+    :return: матрица турнирной игры
+    """
+    return M - M.T
+
+
+def G_reduction(G: np.ndarray, el_to_del: list[int]) -> np.ndarray:
+    """
+    Удаляет из предъявления все альтернативы с индексом из заданного списка. Создает на оставшихся альтернативах
+    новый турнир.
+    :param G: матрица турнирной игры
+    :param el_to_del: список индексов (название альтернативы)
+    :return: матрица турнирной игры (для нового турнира)
+    """
+    G = np.delete(G, el_to_del, 0)
+    return np.delete(G, el_to_del, 1)
+
+
+def DLH(G: np.ndarray) -> dict[str, list[set[int]]]:
+    """
+    Вычисляет верхний и нижний срезы и горизонт каждой альтернативы.
+
+    Верхний срез альтернативы x: D(x) = {y in X | yPx}
+
+    Нижный срез альтернативы x: L(x) = {y in X | xPy}
+
+    Горизонт альтернативы x: H(x) = {y in X | (not xPy) and (not yPx) and (x != y)}
+    :param G: матрица турнирной игры
+    :return: верхний и нижний срезы и горизонт каждой альтернативы предъявления в виде словаря с ключами "D", "L", "H"
+    """
     D = []
     L = []
     H = []
@@ -19,28 +103,47 @@ def DLH(G):
     return {"D": D, "L": L, "H": H}
 
 
-def G_reduction(G, el_to_del):
-    G = np.delete(G, el_to_del, 0)
-    return np.delete(G, el_to_del, 1)
+def CO_2(G: np.ndarray) -> list[int]:
+    """
+    Вычисляет оценки Коупланда s2 (2 версия) для всех альтернатив предъявления.
 
-
-def CO_2(G):
-    # s2(x) = |L(x)|
+    s2(x) = |L(x)|
+    :param G: матрица турнирной игры
+    :return: список оценок Коупланда s2 (2 версия)
+    """
     L = DLH(G)['L']
     s2_list = list(map(len, L))
     return s2_list
 
 
-def CO_3(G):
-    # s3(x) = |X| - |D(x)|
+def CO_3(G: np.ndarray) -> list[int]:
+    """
+    Вычисляет оценки Коупланда s3 (3 версия) для всех альтернатив предъявления.
+
+    s3(x) = |X| - |D(x)|
+    :param G: матрица турнирной игры
+    :return: список оценок Коупланда s3 (3 версия)
+    """
     card_X = len(G)
     D = DLH(G)['D']
     s3_list = [card_X - len(D_x) for D_x in D]
     return s3_list
 
 
-def UC_M(G):
-    # x C_M y <=> xPy and L(y) < L(x)
+def UC_M(G: np.ndarray) -> set[int]:
+    """
+    Вычисляет непокрытое по Миллеру множество UC_M.
+
+    Версия Миллера отношения покрытия:
+
+    x C_M y <=> xPy and L(y) < L(x).
+
+    Альтернатива называется непокрытой по Миллеру тогда и только тогда, когда в предъявлении нет ни одной альтернативы,
+    покрывающей её по Миллеру. Непокрытое по Миллеру множество UC_M есть множество всех непокрытых по версии Миллера
+    отношения покрытия альтернатив.
+    :param G: матрица турнирной игры
+    :return: непокрытое по Миллеру множество UC_M
+    """
     all_alt_set = set(i for i in range(len(G)))
     L = DLH(G)["L"]
     covered = set()
@@ -51,8 +154,20 @@ def UC_M(G):
     return all_alt_set - covered
 
 
-def UC_F(G):
-    # x C_F y <=> xPy and D(x) < D(y)
+def UC_F(G: np.ndarray) -> set[int]:
+    """
+    Вычисляет непокрытое по Фишберну множество UC_F.
+
+    Версия Фишберна отношения покрытия:
+
+    x C_F y <=> xPy and D(x) < D(y).
+
+    Альтернатива называется непокрытой по Фишберну тогда и только тогда, когда в предъявлении нет ни одной альтернативы,
+    покрывающей её по Фишберну. Непокрытое по Фишберну множество UC_F есть множество всех непокрытых по версии Фишберна
+    отношения покрытия альтернатив.
+    :param G: матрица турнирной игры
+    :return: непокрытое по Фишберну множество UC_F
+    """
     all_alt_set = set(i for i in range(len(G)))
     D = DLH(G)["D"]
     covered = set()
@@ -63,8 +178,20 @@ def UC_F(G):
     return all_alt_set - covered
 
 
-def UC_McK(G):
-    # x C_McK y <=> xPy and D(x) < D(y) and L(y) < L(x)
+def UC_McK(G: np.ndarray) -> set[int]:
+    """
+    Вычисляет непокрытое по МакКельви множество UC_McK.
+
+    Версия МакКельви отношения покрытия:
+
+    x C_McK y <=> xPy and D(x) < D(y) and L(y) < L(x).
+
+    Альтернатива называется непокрытой по МакКельви тогда и только тогда, когда в предъявлении нет ни одной
+    альтернативы, покрывающей её по МакКельви. Непокрытое по МакКельви множество UC_McK есть множество всех непокрытых
+    по версии МакКельви отношения покрытия альтернатив.
+    :param G: матрица турнирной игры
+    :return: непокрытое по МакКельви множество UC_McK
+    """
     all_alt_set = set(i for i in range(len(G)))
     L = DLH(G)["L"]
     D = DLH(G)["D"]
@@ -76,8 +203,20 @@ def UC_McK(G):
     return all_alt_set - covered
 
 
-def UC_D(G):
-    # x C_D y <=> H(y) union L(y) < L(x)
+def UC_D(G: np.ndarray) -> set[int]:
+    """
+    Вычисляет непокрытое по Дуггану множество UC_D.
+
+    Версия Дуггана отношения покрытия:
+
+    x C_D y <=> H(y) union L(y) < L(x).
+
+    Альтернатива называется непокрытой по Дуггану тогда и только тогда, когда в предъявлении нет ни одной альтернативы,
+    покрывающей её по Дуггану. Непокрытое по Дуггану множество UC_D есть множество всех непокрытых по версии Дуггана
+    отношения покрытия альтернатив.
+    :param G: матрица турнирной игры
+    :return: непокрытое по Дуггану множество UC_D
+    """
     all_alt_set = set(i for i in range(len(G)))
     L = DLH(G)["L"]
     H = DLH(G)["H"]
@@ -89,8 +228,19 @@ def UC_D(G):
     return all_alt_set - covered
 
 
-def ES(G):
-    # x in ES <=> exists y in UC_F: xPy or x in UC_F
+def ES(G: np.ndarray) -> set[int]:
+    """
+    Вычисляет объединение минимальных внешне устойчивых множеств ES.
+
+    x in ES <=> exists y in UC_F: xPy or x in UC_F
+
+    Алгоритм вычисления взят из статьи:
+
+    Subochev, A. Dominating, weakly stable, and uncovered sets: Properties and generalizations. / A. Subochev //
+    Automation and Remote Control. – 2010. – Vol. 71(1). – p. 116-127.
+    :param G: матрица турнирной игры
+    :return: объединение минимальных внешне устойчивых множеств ES
+    """
     res = set()
     D = DLH(G)["D"]
     UC_F_set = UC_F(G)
@@ -99,8 +249,18 @@ def ES(G):
     return res.union(UC_F_set)
 
 
-def E(G):
-    # E = объединение носителей всех максимальных лотерей
+def E(G: np.ndarray) -> set[int]:
+    """
+    Вычисляет существенное множество E, определяемое как объединение носителей всех максимальных лотерей слабого
+    турнира.
+
+    Алгоритм вычисления взят из статьи:
+
+    Brandt, F. Computing the minimal covering set. / F. Brandt, F. Fischer // Mathematical Social Sciences. – 2008. –
+    Vol. 56(2). – p. 254–268.
+    :param G: матрица турнирной игры
+    :return: существенное множество E
+    """
     list_ind = [i for i in range(len(G))]
     # Вектор коэффициентов целевой функции
     z = [1, *[0 for _ in range(len(G))], -1000, *[0] * (len(G[0]) * 2)]
@@ -142,7 +302,26 @@ def E(G):
     return res
 
 
-def MC_McK(G):
+def MC_McK(G: np.ndarray) -> set[int]:
+    """
+    Вычисляет минимальное покрывающее по МакКельви множество MC_McK.
+
+    В слабом турнире покрывающим по МакКельви множеством Y (Y <= X) называют множество, удовлетворяющее следующим двум
+    условиям:
+
+    1. UC_McK(Y) = Y
+    2. forall x in (X setminus Y), x not in UC_McK(Y cup {x}).
+
+    Минимальное покрывающее по МакКельви множество определяется как единственное минимальное по вложению покрывающее по
+    МакКельви множество.
+
+    Алгоритм вычисления взят из статьи:
+
+    Brandt, F. Computing the minimal covering set. / F. Brandt, F. Fischer // Mathematical Social Sciences. – 2008. –
+    Vol. 56(2). – p. 254–268.
+    :param G: матрица турнирной игры
+    :return: минимальное покрывающее по МакКельви множество MC_McK
+    """
     B = E(G)
     all_alt = set([x for x in range(len(G))])
     while True:
@@ -170,7 +349,24 @@ def MC_McK(G):
             B = B.union(to_add)
 
 
-def MC_D(G):
+def MC_D(G: np.ndarray) -> set[int]:
+    """
+    Вычисляет минимальное покрывающее по Дуггану множество MC_D.
+
+    В слабом турнире покрывающим по Дуггану множеством Y (Y <= X) называют множество, удовлетворяющее следующим двум
+    условиям:
+
+    1. UC_D(Y) = Y
+    2. forall x in (X setminus Y), x not in UC_D(Y cup {x}).
+
+    Минимальное покрывающее по Дуггану множество определяется как единственное минимальное по вложению покрывающее по
+    Дуггану множество.
+
+    Алгоритм вычисления MC_D аналогичен алгоритму вычисления MC_McK (с точностью до переопределения версии
+    отношения покрытия).
+    :param G: матрица турнирной игры
+    :return: минимальное покрывающее по Дуггану множество MC_D
+    """
     B = E(G)
     all_alt = set([x for x in range(len(G))])
     while True:
@@ -198,7 +394,13 @@ def MC_D(G):
             B = B.union(to_add)
 
 
-def sorting(G, func):
+def sorting(G: np.ndarray, func: Callable[[np.ndarray], Union[set[int], list[int]]]) -> list[int]:
+    """
+    Производит сортировку альтернатив из предъявления по правилу, заданному функцией func.
+    :param G: матрица турнирной игры
+    :param func: турнирное решение
+    :return: ранжировка, полученная путем сортировки альтернатив заданным турнирным решением
+    """
     rank_all = []
     N = len(G)
     if func in [CO_2, CO_3]:
@@ -239,44 +441,29 @@ def sorting(G, func):
     return ranking
 
 
-def init_ranking(df):
-    rankings = list(df.columns)
-    for ranking in rankings:
-        res = np.zeros(len(df))
-        place = 1
-        maximum = df[ranking].max()
-        while maximum != -100:
-            ind_list = np.where(df[ranking] == maximum)
-            for ind in ind_list:
-                df[ranking][ind] = -100
-                res[ind] = place
-            place += 1
-            maximum = df[ranking].max()
-        df[ranking] = res
-    df = df.astype("int")
-    return df
-
-
-def M_T_generator(df):
-    values = df.values
-    M = np.zeros((values.shape[0], values.shape[0]))
-    T = np.zeros((values.shape[0], values.shape[0]))
-    num = 0
-    for i in range(values.shape[0]):
-        for j in range(values.shape[0]):
-            counter = 0
-            for k in range(1, values.shape[1]):  # идем по ранжированиям
-                if values[i][k] < values[j][k]:
-                    counter += 1
-                elif values[i][k] > values[j][k]:
-                    counter -= 1
-            if np.sign(counter) == 1:
-                M[i][j] = 1
-            elif np.sign(counter) == 0 and i != j:
-                T[i][j] = 1
-                num += 1
-    return M.astype(np.int64), T.astype(np.int64), num // 2
-
-
-def game_creator(M):
-    return M - M.T
+def custom_format(writer: ExcelWriter, mode: str, rotation: int = 0) -> ExcelWriter:
+    """
+    Создает шаблон для форматирования чисел и заголовков.
+    :param writer: объект, записывающий данные на лист Excel (с исходным форматированнием)
+    :param mode: тип форматируемых значений. "base" - базовый, "header_col" - заголовок (для столбцов),
+    "header_row" - заголовок (для строк)
+    :param rotation: угол поворота текста (в градусах) (значение по умолчанию: 0)
+    :return: объект, записывающий данные на лист Excel (с измененным форматированием)
+    """
+    style_dict = {
+        "font_name": "Times New Roman",
+        "font_size": 12,
+        "bold": False,
+        "align": "center",
+        "valign": "center"
+    }
+    match mode:
+        case "base":
+            return writer.book.add_format(style_dict)
+        case "header_col":
+            style_dict["rotation"] = rotation
+            return writer.book.add_format(style_dict)
+        case "header_row":
+            style_dict["align"] = "left"
+            style_dict["valign"] = "left"
+            return writer.book.add_format(style_dict)
